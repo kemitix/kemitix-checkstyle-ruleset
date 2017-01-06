ab0a95e3950faf638a9983d831980fd478ac4bce
Paul Campbell <pcampbell@kemitix.net>
2017-01-06 21:56:37 +0000

    builder:RulesProperties: initialise rules list

--- builder/src/main/java/net/kemitix/checkstyle/ruleset/builder/RulesProperties.java
+++ builder/src/main/java/net/kemitix/checkstyle/ruleset/builder/RulesProperties.java
@@ -30,6 +30,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -47,5 +48,5 @@ class RulesProperties {
     /**
      * The Checkstyle checks.
      */
-    private List<Rule> rules;
+    private List<Rule> rules = new ArrayList<>();
 }
