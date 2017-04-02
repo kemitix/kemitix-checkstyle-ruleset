8b1627df0b9b6f237b690306b798c7b04e2cd2fb
Paul Campbell <pcampbell@kemitix.net>
2017-04-02 13:45:59 +0100

    LICENSE.txt: updated

--- regressions/src/main/java/net/kemitix/checkstyle/regressions/package-info.java
+++ regressions/src/main/java/net/kemitix/checkstyle/regressions/package-info.java
@@ -17,16 +17,6 @@
  * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * .
- *
- * @author Paul Campbell (pcampbell@kemitix.net)
- */
-
-/**
- * .
- *
- * @author Paul Campbell (pcampbell@kemitix.net)
  */
 
 package net.kemitix.checkstyle.regressions;
