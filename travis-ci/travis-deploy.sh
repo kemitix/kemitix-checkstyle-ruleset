883aa4342e5d4a54ae443f9ddadc714abbad1668
Paul Campbell <pcampbell@kemitix.net>
2017-05-28 17:25:20 +0100

    travis-ci: replace signing key to match passphrase

--- travis-ci/travis-deploy.sh
+++ travis-ci/travis-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_e3200738ab3d_key -iv $encrypted_e3200738ab3d_iv \
+	openssl aes-256-cbc -K $encrypted_efec3258f55d_key -iv $encrypted_efec3258f55d_iv \
 		-in travis-ci/codesigning.asc.enc -out travis-ci/codesigning.asc -d
 	gpg --fast-import travis-ci/codesigning.asc
 	./mvnw --projects plugin,ruleset --settings travis-ci/travis-settings.xml \
