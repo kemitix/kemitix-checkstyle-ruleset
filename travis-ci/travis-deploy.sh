c8578b46597dfe67d6e81633911030491ed7910b
Paul Campbell <pcampbell@kemitix.net>
2017-05-28 16:29:56 +0100

    travis-ci: update location of travis-settings.xml

--- travis-ci/travis-deploy.sh
+++ travis-ci/travis-deploy.sh
@@ -4,6 +4,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
 	openssl aes-256-cbc -K $encrypted_e3200738ab3d_key -iv $encrypted_e3200738ab3d_iv \
 		-in travis-ci/codesigning.asc.enc -out travis-ci/codesigning.asc -d
 	gpg --fast-import travis-ci/codesigning.asc
-	./mvnw --projects plugin,ruleset --settings travis-settings.xml -Dskip-Tests=true -P release -B deploy
+	./mvnw --projects plugin,ruleset --settings travis-ci/travis-settings.xml \
+		-Dskip-Tests=true -P release -B deploy
 fi
 
