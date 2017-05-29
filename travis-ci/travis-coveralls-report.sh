7229c11b30643137d99966e876e766aadd80fae1
Paul Campbell <pcampbell@kemitix.net>
2017-05-29 12:04:43 +0100

    KCR29: travis-ci: detect Travis-CI

--- travis-ci/travis-coveralls-report.sh
+++ travis-ci/travis-coveralls-report.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "TRAVIS: $TRAVIS"
-set
-if [ "$TRAVIS" = "true" ];then
+## Only send coveralls reports from Travis-CI. Some CIs, like Shippable, lie by setting TRAVIS=true.
+## Currently, Shippable, does not set TRAVIS_LANGUAGE, but Travis-CI does.
+if [ "$TRAVIS_LANGUAGE" = "java" ];then
     ./mvnw --projects builder,plugin test jacoco:report coveralls:report
 fi
