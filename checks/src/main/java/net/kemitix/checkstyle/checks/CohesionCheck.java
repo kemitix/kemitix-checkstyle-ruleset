a4730da7a057bd4467f3213de6e0f6c132faa01d
Paul Campbell <pcampbell@kemitix.net>
2017-06-02 19:35:09 +0100

    CohesionCheck: empty class with intended purpose documented

---
+++ checks/src/main/java/net/kemitix/checkstyle/checks/CohesionCheck.java
@@ -0,0 +1,17 @@
+package net.kemitix.checkstyle.checks;
+
+/**
+ * Checkstyle Check to ensure appropriate cohesion within a class of its methods and fields.
+ *
+ * <p>Specifically it measures the ratio of fields used by each public method.</p>
+ *
+ * <p>If this ratio falls below a threshold, then a violation will be logged.</p>
+ *
+ * <p>It also tried to detect when there is a partitioning of methods and fields indicating that they should probably be
+ * split into a separate class.</p>
+ *
+ * @author Paul Campbell (pcampbell@kemitix.net).
+ */
+public class CohesionCheck {
+
+}
---
+++ checks/src/test/java/net/kemitix/checkstyle/checks/CohesionCheckTest.java
@@ -0,0 +1,12 @@
+package net.kemitix.checkstyle.checks;
+
+/**
+ * Tests for {@link CohesionCheck}.
+ *
+ * @author Paul Campbell (pcampbell@kemitix.net).
+ */
+public class CohesionCheckTest {
+
+    private CohesionCheck check;
+
+}
