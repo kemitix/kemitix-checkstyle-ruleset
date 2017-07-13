90e3652e6e8c6412aba22ff39a36a6ed5b2244ad
Paul Campbell <pcampbell@kemitix.net>
2017-07-13 14:19:39 +0100

    {builder,plugin}:*Test: replace deprecated org.mockito.Matchers.* imports

--- builder/src/test/java/net/kemitix/checkstyle/ruleset/builder/CheckstyleWriterTest.java
+++ builder/src/test/java/net/kemitix/checkstyle/ruleset/builder/CheckstyleWriterTest.java
@@ -22,8 +22,8 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.any;
 
 /**
  * Tests for {@link CheckstyleWriter}.
