a79cfc1f1f13a2314d009c7f9062d79d6347f0d8
Paul Campbell <pcampbell@kemitix.net>
2018-01-24 21:56:07 +0000

    Add set-version.sh

---
+++ set-version.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if test $# != 2
+then
+    echo "current and next version missing"
+    exit
+fi
+
+CURRENT=$1
+NEXT=$2
+
+echo Updating version from $CURRENT to $NEXT
+
+./mvnw versions:set -DnewVersion=$NEXT
+perl -p -i -e "s,$CURRENT</,$NEXT</," builder/pom.xml builder/src/main/resources/README-template.md regressions/pom.xml ruleset/pom.xml tile/pom.xml tile/tile.xml
