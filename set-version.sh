99fea198227231ed596d40bf994d6dd5ffe8bfba
Paul Campbell <pcampbell@kemitix.net>
2018-10-21 18:18:56 +0100

    [bug] tile depends on DEV-SNAPSHOT ruleset

--- set-version.sh
+++ set-version.sh
@@ -11,5 +11,5 @@ NEXT=$1
 echo "Updating version to $NEXT..."
 mvn versions:set -DnewVersion=$NEXT -DgenerateBackupPoms=false -DprocessAllModules
 echo "Updating README template..."
-perl -p -i -e "s,DEV-SNAPSHOT</,$NEXT</," builder/src/main/resources/README-template.md
+perl -p -i -e "s,DEV-SNAPSHOT</,$NEXT</," builder/src/main/resources/README-template.md tile/pom.xml
 echo "Done."
